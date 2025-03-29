22274b15fcf7f3555af7d37418c1b91cfdb94466
sahid16 <sahidmalek958@gmail.com>
2025-03-29 10:38:35 +0530

    printing and comments

---
+++ print.py
@@ -0,0 +1,13 @@
+print("sahid")#for printing a name but you can print anything you write between in ("here")
+
+print("") #for ctreate emty line
+
+print("hi")#this code print hi
+
+#this is a single line comment
+
+"""and this is a multiline comment"""
+
+"we write comment for giving a messge to reader or exain our code"
+
+#and you can write anything important in comment  python didn't exicute anything  frome comment you wrote
